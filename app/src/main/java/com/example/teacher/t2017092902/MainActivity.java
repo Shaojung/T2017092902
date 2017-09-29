4a0f1c57d32aa6384545e6c8e31856f5a34bd99f
Shaojung Lu <shaojung@gmail.com>
2017-09-29 14:21:36 +0800

    use Thread to show circle 2 seconds.

--- app/src/main/java/com/example/teacher/t2017092902/MainActivity.java
+++ app/src/main/java/com/example/teacher/t2017092902/MainActivity.java
@@ -1,13 +1,42 @@
 package com.example.teacher.t2017092902;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
-
+    ProgressBar pb;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        pb = (ProgressBar) findViewById(R.id.progressBar);
+    }
+    public void click1(View v)
+    {
+        pb.setVisibility(View.VISIBLE);
+    }
+    public void click2(View v)
+    {
+        pb.setVisibility(View.INVISIBLE);
+    }
+    public void click3(View v)
+    {
+        pb.setVisibility(View.VISIBLE);
+        new Thread(){
+            @Override
+            public void run()
+            {
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                pb.setVisibility(View.INVISIBLE);
+            }
+        }.start();
+
+
     }
 }
