ce3941ca51f240dda63b79b40520ffa764a4a8aa
Shaojung Lu <shaojung@gmail.com>
2017-09-29 14:59:34 +0800

    use progressbar

--- app/src/main/java/com/example/teacher/t2017092902/MainActivity.java
+++ app/src/main/java/com/example/teacher/t2017092902/MainActivity.java
@@ -6,12 +6,13 @@ import android.view.View;
 import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
-    ProgressBar pb;
+    ProgressBar pb, pb2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         pb = (ProgressBar) findViewById(R.id.progressBar);
+        pb2 = (ProgressBar) findViewById(R.id.progressBar2);
     }
     public void click1(View v)
     {
@@ -33,10 +34,18 @@ public class MainActivity extends AppCompatActivity {
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-                pb.setVisibility(View.INVISIBLE);
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        pb.setVisibility(View.INVISIBLE);
+                    }
+                });
+
             }
         }.start();
-
-
+    }
+    public void click4(View v)
+    {
+        pb2.setProgress(pb2.getProgress() + 10);
     }
 }
